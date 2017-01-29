b9c889e2a03f76ab48d2a032359010179e0ac1c7
notdls <dlsvuln@gmail.com>
2017-01-29 20:58:49 +1100

    Added Error checking and sleeping

--- ddos_no_vbrk.py
+++ ddos_no_vbrk.py
@@ -1,6 +1,17 @@
 # DDOS NO VBRK REQUIRED #
+# GreeTz FrOm SeCrEt SoCiEtY #
 import requests
+import time
 sessID = input("PHPSESSID >> ")
-target = input("TARGET IP >> ")
-cookies = dict(PHPSESSID=sessID)
-r = requests.post("https://legacy.hackerexperience.com/DDoS", data = {'ip':target}, cookies=cookies)
+while True:
+	target = input("TARGET IP >> ")
+	cookies = dict(PHPSESSID=sessID)
+	r = requests.post("https://legacy.hackerexperience.com/DDoS", data = {'ip':target}, cookies=cookies) # SEND POST REQUEST
+	if "Hacker Experience is a browser-based hacking simulation game" in r.text: # CHECK IF THE SESSION IS VALID
+		print ("Error: Invalid PHPSESSID")
+		exit()
+	elif "Launch DDoS attack" in r.text: # CHECK IF THERE WAS AN ERROR
+		print ("Error: There was an issue launching the DDoS Attack, please make sure the IP is valid and alive")
+	else:		
+		print ("DDoS Attack Launched Against {} | Waiting 5 minutes...".format(target))
+		time.sleep(300)
